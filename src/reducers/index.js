dbc2c7de657981845917b0c9c99928af3d8caca6
Dan Sherman <dvsherman@protonmail.com>
2019-03-04 16:25:20 -0700

    completed mvp, moving to stretch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,9 +12,15 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return {count: state.count +1};
+      return {
+        count: state.count +1
+      };
+
     case DECREMENT:
-      return {count: state.count -1};
+      return {
+        count: state.count -1
+      };
+      
     default:
       return state;
   }
